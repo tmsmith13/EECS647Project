0926898cd84010b3583d04e1d4570161d5419e65
Trevor Smith <tsmith13@ku.edu>
2011-11-09 21:41:19 -0600

    Changed WEB to web.

---
+++ web/index.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+
+<div class="form">
+<form action="process.php" method="post">
+carVIN: <input type="text" name="VIN" />
+type: <input type="text" name="BodyType" />
+<input type="submit" />
+</form>
+</div>
+
+</body>
+</html>
