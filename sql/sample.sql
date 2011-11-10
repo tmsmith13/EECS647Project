641207e1a8d767d347993ba8ec1ecd39698f6eeb
Trevor Smith <tsmith13@ku.edu>
2011-11-10 00:39:36 -0600

    Added vehicles and features tables and entered a row for each in sample.sql

--- sql/sample.sql
+++ sql/sample.sql
@@ -23,3 +23,61 @@ INSERT INTO customers(
     12345,
     '316-321-4567'
 );
+
+SET foreign_key_checks = 0; -- Has to be set or insert fails
+
+INSERT INTO vehicles(
+    model_id,
+    engine_id,
+    transmission_id,
+    brake_id,
+    vin,
+    year,
+    vehicle_condition,
+    body_color,
+    hwy_mpg,
+    city_mpg,
+    fuel_tank_size,
+    dealer_purchase_price,
+    advertised_sale_price,
+    miles
+) VALUES (
+    1,
+    1,
+    1,
+    1,
+    '1234M5K6789',
+    2011,
+    'new',
+    'black',
+    7,
+    5,
+    2,
+    12000.50,
+    26000.75,
+    5
+);
+
+INSERT INTO features(
+    power_door,
+    keyless_entry,
+    rear_view_camera,
+    gps,
+    power_window,
+    satellite_radio,
+    hybrid,
+    window_tint,
+    sunroof
+) VALUES (
+    1,
+    1,
+    0,
+    1,
+    1,
+    0,
+    0,
+    1,
+    1
+);
+
+SET foreign_key_checks = 1;
--- sql/schema.sql
+++ sql/schema.sql
@@ -24,4 +24,59 @@ CREATE TABLE customers (
 ) ENGINE=InnoDB
 ;
 
+DROP TABLE IF EXISTS vehicles;
+CREATE TABLE vehicles (
+    vehicle_id            INT             NOT NULL    AUTO_INCREMENT PRIMARY KEY,
+    model_id              INT             NOT NULL,
+    engine_id             INT             NOT NULL,
+    transmission_id       INT             NOT NULL,
+    brake_id              INT             NOT NULL,
+    vin                   VARCHAR(25)     NOT NULL,
+    year                  INT UNSIGNED    NOT NULL,
+    vehicle_condition     VARCHAR(12)     NOT NULL,
+    body_color            VARCHAR(20)     NOT NULL,
+    hwy_mpg               FLOAT           NOT NULL,
+    city_mpg              FLOAT           NOT NULL,
+    fuel_tank_size        INT UNSIGNED    NOT NULL,
+    dealer_purchase_price FLOAT           NOT NULL,
+    advertised_sale_price FLOAT           NOT NULL,
+    miles                 INT UNSIGNED    NOT NULL,
+    INDEX (vin),
+    INDEX (model_id), -- Might not be needed
+    FOREIGN KEY (vehicle_id)
+        REFERENCES features(vehicle_id)
+        ON UPDATE CASCADE ON DELETE RESTRICT,
+    FOREIGN KEY (model_id)
+        REFERENCES models(model_id)
+        ON UPDATE CASCADE ON DELETE RESTRICT,
+    FOREIGN KEY (engine_id)
+        REFERENCES engines(engine_id)
+        ON UPDATE CASCADE ON DELETE RESTRICT,
+    FOREIGN KEY (model_id)
+        REFERENCES transmissions(transmission_id)
+        ON UPDATE CASCADE ON DELETE RESTRICT,
+    FOREIGN KEY (brake_id)
+        REFERENCES brakes(brake_id)
+        ON UPDATE CASCADE ON DELETE RESTRICT
+) ENGINE=InnoDB
+;
+
+DROP TABLE IF EXISTS features;
+CREATE TABLE features (
+    vehicle_id       INT             NOT NULL    AUTO_INCREMENT PRIMARY KEY,
+    power_door       BOOLEAN         NOT NULL,
+    keyless_entry    BOOLEAN         NOT NULL,
+    rear_view_camera BOOLEAN         NOT NULL,
+    gps              BOOLEAN         NOT NULL,
+    power_window     BOOLEAN         NOT NULL,
+    satellite_radio  BOOLEAN         NOT NULL,
+    hybrid           BOOLEAN         NOT NULL,
+    window_tint      BOOLEAN         NOT NULL,
+    sunroof          BOOLEAN         NOT NULL,
+    FOREIGN KEY (vehicle_id)
+        REFERENCES vehicles(vehicle_id)
+        ON UPDATE CASCADE ON DELETE RESTRICT
+) ENGINE=InnoDB
+;
+
 SET foreign_key_checks = 1;
