3a2439661b3725e4044734644db78bc43241fee6
Jason E <jason.e@joes.dealership.com>
2011-11-15 09:37:53 -0600

    Fix customers sample data
    * Load data in file not formatted correctly, so all fields were being inserted
      into first_name.
    * Remove email addresses - there is no email in customers.
    * Commented out load infile for non-existent files.

--- sql/sampleDataLoader.sql
+++ sql/sampleDataLoader.sql
@@ -1,27 +1,34 @@
-LOAD DATA LOCAL INFILE 'customers.csv'          INTO TABLE customers(first_name,last_name,date_of_birth,street,zip,phone_number);
+SET foreign_key_checks = 0;
 
-LOAD DATA LOCAL INFILE 'locations.csv'          INTO TABLE locations(zip,city,state);
+LOAD DATA LOCAL INFILE 'customers.csv'
+INTO TABLE customers
+FIELDS TERMINATED BY ','
+(first_name,last_name,date_of_birth,street,zip,phone_number);
 
-LOAD DATA LOCAL INFILE 'employees.csv'          INTO TABLE employees(first_name,last_name,password,ssn,date_of_birth,email,street,zip,phone_number);
+-- LOAD DATA LOCAL INFILE 'locations.csv'          INTO TABLE locations(zip,city,state);
 
-LOAD DATA LOCAL INFILE 'brakes.csv'             INTO TABLE brakes;
+-- LOAD DATA LOCAL INFILE 'employees.csv'          INTO TABLE employees(first_name,last_name,password,ssn,date_of_birth,email,street,zip,phone_number);
 
-LOAD DATA LOCAL INFILE 'transmissions.csv'      INTO TABLE transmissions;
+-- LOAD DATA LOCAL INFILE 'brakes.csv'             INTO TABLE brakes;
 
-LOAD DATA LOCAL INFILE 'engines.csv'            INTO TABLE engines;
+-- LOAD DATA LOCAL INFILE 'transmissions.csv'      INTO TABLE transmissions;
 
-LOAD DATA LOCAL INFILE 'makes.csv'              INTO TABLE makes;
+-- LOAD DATA LOCAL INFILE 'engines.csv'            INTO TABLE engines;
 
-LOAD DATA LOCAL INFILE 'models.csv'             INTO TABLE models;
+-- LOAD DATA LOCAL INFILE 'makes.csv'              INTO TABLE makes;
 
-LOAD DATA LOCAL INFILE 'vehicles.csv'           INTO TABLE vehicles;
+-- LOAD DATA LOCAL INFILE 'models.csv'             INTO TABLE models;
 
-LOAD DATA LOCAL INFILE 'sales.csv'              INTO TABLE sales;
+-- LOAD DATA LOCAL INFILE 'vehicles.csv'           INTO TABLE vehicles;
 
-LOAD DATA LOCAL INFILE 'transactions.csv'       INTO TABLE transactions;
+-- LOAD DATA LOCAL INFILE 'sales.csv'              INTO TABLE sales;
 
-LOAD DATA LOCAL INFILE 'card_transactions.csv'  INTO TABLE card_transactions;
+-- LOAD DATA LOCAL INFILE 'transactions.csv'       INTO TABLE transactions;
 
-LOAD DATA LOCAL INFILE 'cash_transactions.csv'  INTO TABLE cash_transactions;
+-- LOAD DATA LOCAL INFILE 'card_transactions.csv'  INTO TABLE card_transactions;
 
-LOAD DATA LOCAL INFILE 'check_transactions.csv' INTO TABLE check_transactions;
+-- LOAD DATA LOCAL INFILE 'cash_transactions.csv'  INTO TABLE cash_transactions;
+
+-- LOAD DATA LOCAL INFILE 'check_transactions.csv' INTO TABLE check_transactions;
+
+SET foreign_key_checks = 1;
