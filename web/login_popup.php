aa7fb26c342a2fdb876219e7ca42fba80c3216bc
Jason E <jason.e@joes.dealership.com>
2011-12-05 12:31:01 -0600

    Make login box stay inside viewport

--- web/login_popup.php
+++ web/login_popup.php
@@ -1,7 +1,11 @@
    <div id="popupbox" style="text-align:center"> 	
 		<form name="Login" action="login.php" method="post">
-			Username: <input name="username" size="25" />
-			Password: <input name="password" type="password" size="25" />
+			<div>
+				Username: <input name="username" size="25" />
+			</div>
+			<div>
+				Password: <input name="password" type="password" size="25" />
+			</div>
 			<input type="submit" name="submit" value="Login" />
 		</form>
 		<p id="result"></p>
