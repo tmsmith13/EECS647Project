b195adfb503287fc06a6a5cf3c8671a2ba1991f6
Jason Held <jasonsheld@gmail.com>
2011-12-05 14:16:48 -0600

    Adding my sample query ideas. Will populate with SQL soon.

---
+++ sql/jh_queries.sql
@@ -0,0 +1,10 @@
+-- Transactions between certain dates, a certain type, and a certain amount
+
+-- Commission earned by employees between do dates, in ascending order
+
+-- The number of sales per month in ascending order
+
+-- The number of fords sold within a given price range
+
+-- Return the five cheapest cars
+
