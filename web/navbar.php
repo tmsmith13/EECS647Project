e3022300c883f16850d3f6dea182633d73d27e67
Jason E <jason.e@joes.dealership.com>
2011-12-04 23:25:50 -0600

    Add navbar
    Currently only on sales page.  Can be added to other pages.

---
+++ web/navbar.php
@@ -0,0 +1,6 @@
+<div id="main_nav">
+	<ul class="navbar">
+		<li><a href="<?= $app_path ?>">Vehicles Search</a>
+		<li><a href="<?= $app_path ?>sales.php">Recent Sales</a>
+	</ul>
+</div>
--- web/page_header.php
+++ web/page_header.php
@@ -3,6 +3,7 @@
 <head>
 	<title><? if (isset($title)): ?><?= $title ?> - <? endif ?>Billy Joe's Car Dealership</title>
 	<link rel="stylesheet" href="<?= $app_path ?>css/master.css" type="text/css" media="screen" />
+	<link rel="stylesheet" href="<?= $app_path ?>css/navbar.css" type="text/css" media="screen" />
 	<link rel="stylesheet" href="<?= $app_path ?>css/pretty_form.css" type="text/css" media="screen" />
 
 	<script type="text/javascript" src="<?= $app_path ?>js/jquery.min.js" charset="utf-8"></script>
@@ -14,5 +15,7 @@
 		<div class="title_container">
 			<p id="logo">The Bug Makers Car Dealership</p>
 		</div> <!--end title_conatiner-->
+
+		<? include('navbar.php') ?>
 
 		<div class="main_body_container">
