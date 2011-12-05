db426a3ef5a3ee3707e9684cc6a90e8b963a4436
Jason Held <jasonsheld@gmail.com>
2011-12-05 16:45:43 -0600

    Added some sample queries to jh_queries.sql

--- sql/jh_queries.sql
+++ sql/jh_queries.sql
@@ -1,10 +1,24 @@
--- Transactions between certain dates, a certain type, and a certain amount
+-- Transactions between certain dates, card transaction, payment_amount constraint, order descending
+SELECT transactions.*, transaction_type
+FROM (
+	 SELECT transaction_id, 'card transaction' AS transaction_type
+) trans
+NATURAL JOIN transactions
+WHERE transaction_date BETWEEN '2011-11-11' AND '2011-11-23' AND payment_amount > 100
+ORDER BY -payment_amount;
 
--- Commission earned by employees between do dates, in ascending order
+-- Commission earned by employees between two dates, in ascending order
+SELECT employee_id, SUM(commission) 
+FROM sales 
+WHERE sale_date BETWEEN '2011-11-01' AND '2011-11-25' GROUP BY employee_id;
 
 -- The number of sales per month in ascending order
+SELECT COUNT(sales_date), MONTH(sales_date)
+FROM sales
+ORDER BY COUNT(sales_date);
 
 -- The number of fords sold within a given price range
+
 
 -- Return the five cheapest cars
 
